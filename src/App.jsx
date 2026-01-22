caf1680dda812f8b6be4c9d09daf5c23ac33be34
AnkitBarmola <ankitbarmola1815@gmail.com>
2026-01-22 18:24:42 +0530

    added the API and the page based navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from './components/Card';
 
 const App = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  const [index, setIndex] = useState(1)
+
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=21`)
+    setUserData(response.data)
+  }
+
+  useEffect(function () {
+    getData()
+  }, [index])
+
+
+  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+
+      return <div key={idx}>
+        <Card elem={elem} />
+      </div>
+    })
+  }
+
   return (
-     <div className='bg-black overflow-auto h-screen p-4 text-white'>
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
       <div className='flex h-[82%] flex-wrap gap-4 p-2'>
         {printUserData}
       </div>
@@ -38,4 +66,3 @@ const App = () => {
 }
 
 export default App
-f
